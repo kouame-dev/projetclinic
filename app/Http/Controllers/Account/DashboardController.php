75568a5d75cd1ff3c3018fbcf03d78b23f917f10
kouame-dev <koua.dibi@gmail.com>
2025-02-07 08:55:58 +0100

    Initial commit

---
+++ app/Http/Controllers/Account/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Account;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function __construct()
+    {
+        $allowedGuards = implode(',', array_keys(config('auth.guards')));
+        $this->middleware(['auth:' . $allowedGuards]);
+        
+    }
+
+    public function index(Request $request){
+//dd($request);
+        return view('account.dashboard');
+    }
+}
---
+++ app/Http/Controllers/Account/DentistController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Account;
+
+use App\Http\Controllers\Controller;
+use App\Models\TreatmentPlan;
+use Illuminate\Http\Request;
+
+class DentistController extends DashboardController
+{
+    /**
+     * Get a  given dentist treatment plans
+     */
+    
+	
+    public function getTreatmentPlans(Request $request)
+    {
+        $treatmentPlans = collect([]);
+        if (principal()->isDentist()) {
+            $treatmentPlans = principal()->treatmentPlans;
+        }
+
+        return view('account.dentist.plans.index', [
+            'plans' => $treatmentPlans,
+        ]);
+    }
+	
+		
+    public function getPatient(Request $request)
+    {
+        $treatmentPlans = collect([]);
+        if (principal()->isDentist()) {
+            $treatmentPlans = principal()->treatmentPlans;
+        }
+
+        return view('account.dentist.plans.patient', [
+            'plans' => $treatmentPlans,
+        ]);
+    }
+}
